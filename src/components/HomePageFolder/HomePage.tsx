61d9fb37f8e2ac5a9d1b120466ff03eafc6318a4
Pedro Castaneda <petecast76@gmail.com>
2023-05-07 02:09:32 -0700

    Changed layout for projects

--- src/components/HomePageFolder/HomePage.tsx
+++ src/components/HomePageFolder/HomePage.tsx
@@ -10,7 +10,11 @@ import BootstrapIcon from '../../assets/bootstrap.svg';
 import ReactIcon from '../../assets/atom.svg';
 import TypeScriptIcon from '../../assets/typescript.svg';
 import CSharp from '../../assets/c-sharp.svg';
+import TailwindIcon from '../../assets/tailwindIcon.png'
 import AzureIcon from '../../assets/azure.svg';
+import PokeballImg from '../../assets/pokeball.svg';
+import PokeballFavorites from '../../assets/pokeFavorites.png';
+import PokeballHomepage from '../../assets/pokeHomepage.png';
 import './HomePage.css';
 
 const HomePage = () => {
@@ -18,50 +22,109 @@ const HomePage = () => {
     return (
         <>
             <div className="flex flex-col h-screen inder">
-                <div className="flex-grow">
-                    <Navbar />
+                <Navbar />
+                <div className="flex-grow container mx-auto">
+
                     <div className="relative container mx-24 mt-48 w-auto">
                         <div className='text-9xl lightNeonBlue font-bold nunito'>Hi there,</div>
-                        <p className='smokeyGray w-1/2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti nulla earum totam nobis? Qui reprehenderit neque rem soluta ratione, quibusdam est veritatis obcaecati culpa illo perferendis eveniet, iste in rerum!</p>
-                        <img className='absolute top-0 right-0 w-auto h-72 grayscale hover:grayscale-0 mr-24' src={Headshot} alt="Picture of me, Pedro" />
+                        <p className='smokeGreyText Text w-1/2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti nulla earum totam nobis? Qui reprehenderit neque rem soluta ratione, quibusdam est veritatis obcaecati culpa illo perferendis eveniet, iste in rerum!</p>
+                        <img className='absolute top-0 right-0 w-auto h-72 grayscale hover:grayscale-0 mr-24 rounded-full' src={Headshot} alt="Picture of me, Pedro" />
                     </div>
                     <div className='container mx-24 w-auto mt-36'>
                         <p className='lightNeonBlue font-bold text-4xl mb-8 nunito'>Projects</p>
-                        <div className="container mx-auto w-auto px-6 smokeyGray">
-                            <div className="grid grid-cols-2 gap-0">
-                                <button className='h-72 relative flex flex-col items-center w-4/5 hover:shadow-lg hover:shadow-teal-300 p-2 hover:animate-pulse rounded-xl'>
-                                    <img className='h-auto w-40 mt-6' src={FleetFinderIcon} alt="Fleet Finder icon" />
-                                    <p className='mt-4'>Fleet Finder</p>
-                                    <p className='text-sm mt-8 lightNeonBlue'>click to view website</p>
-                                </button>
+                        <div className="container mx-auto w-auto px-10 smokeyGrayText">
+                            {/* Project #1 */}
+                            <div className="grid grid-cols-2">
+                                <div className='h-72 flex flex-col items-center w-full'>
+                                    <div className='flex flex-row'>
+                                        <img className='h-auto w-24 mt-6' src={FleetFinderIcon} alt="Fleet Finder icon" />
+                                        <p className='mt-4 self-center text-4xl'>Fleet Finder</p>
+                                    </div>
+                                    <div className="flex flex-row space-x-2">
+                                        <a href='#' className='text-sm mt-8 lightNeonBlue hover:animate-pulse rounded-md hover:shadow-lg hover:shadow-teal-300 p-2'>view website</a>
+                                        <a href='https://github.com/rooted92/frontendFF' target='_blank' className='text-sm mt-8 lightNeonBlue hover:animate-pulse rounded-md hover:shadow-lg hover:shadow-teal-300 p-2'>view frontend code</a>
+                                        <a href='https://github.com/rooted92/backendFF' target='_blank' className='text-sm mt-8 lightNeonBlue hover:animate-pulse rounded-md hover:shadow-lg hover:shadow-teal-300 p-2'>view backend code</a>
+                                    </div>
+                                </div>
+                                <div className='flex flex-col row-span-2'>
+                                    <p className='font-bold text-xl smokeGreyText'>Description</p>
+                                    <p className='smokeyGrayText'>Fleet Finder is a project that improves communication and operational efficiency in the transportation industry. It simplifies the process of notifying dispatch teams about the location and important details of company trailers, benefiting truck drivers and dispatch teams alike. Using Fleet Finder, truck drivers can submit a form that provides real-time updates on trailer status and key information, which can be immediately viewed by the dispatch team.</p>
+                                    <p className='font-bold text-xl mt-3 smokeGreyText'>Built with</p>
+                                    <div className="grid grid-cols-5 p-4 mt-2">
+                                        <div className="flex flex-col justify-center">
+                                            <img className='h-14 w-auto' src={ReactIcon} alt="react icon" />
+                                            <p className='text-center'>react.js</p>
+                                        </div>
+                                        <div className="flex flex-col justify-center">
+                                            <img className='h-14 w-auto' src={TypeScriptIcon} alt="typescript icon" />
+                                            <p className='text-center'>typescript</p>
+                                        </div>
+                                        <div className="flex flex-col justify-center">
+                                            <img className='h-14 w-auto' src={BootstrapIcon} alt="bootstrap icon" />
+                                            <p className='text-center'>bootstrap</p>
+                                        </div>
+                                        <div className="flex flex-col justify-center">
+                                            <img className='h-14 w-auto' src={CSharp} alt="c-sharp icon" />
+                                            <p className='text-center'>c-sharp</p>
+                                        </div>
+                                        <div className="flex flex-col justify-center">
+                                            <img className='h-14 w-auto' src={AzureIcon} alt="azure icon" />
+                                            <p className='text-center'>azure</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* <div className='col-span-1'>
+                                    <div className=''>
+                                        <img src={FleetFinderLoginImg} alt="image of Fleet Finder login page" />
+                                    </div>
+                                    <div className=''>
+                                        <img src={FleetFinderCode} alt="image of Dispatch Dashboard from Fleet Finder app" />
+                                    </div>
+                                    <div className='overflow-y-scroll h-72'>
+                                        <img className='w-full h-auto' src={DispatchDashboardImg} alt="image of code from Fleet Finder" />
+                                    </div>
+                                </div> */}
+                            </div>
+
+                            <hr className=' border-teal-300 rounded-3xl my-8' />
+                            {/* Project #2 */}
+                            <div className="grid grid-cols-2">
+                                <div className='min-h-72 relative flex flex-col pt-20 items-center w-full p-2 '>
+                                    <p className='brownFont text-6xl flex flex-row quattroFont'>P<img className='w-8 h-8 mt-4' src={PokeballImg} alt="pokeball" />kedex</p>
+                                    <div className="flex flex-row space-x-2">
+                                        <a href='#' className='text-sm mt-8 lightNeonBlue hover:animate-pulse rounded-md hover:shadow-lg hover:shadow-teal-300 p-2'>view website</a>
+                                        <a href='https://github.com/rooted92/pokedexreact' target='_blank' className='text-sm mt-8 lightNeonBlue hover:animate-pulse rounded-md hover:shadow-lg hover:shadow-teal-300 p-2'>view code</a>
+                                    </div>
+                                </div>
                                 <div className='flex flex-col'>
-                                    <p className='font-bold text-xl'>Description</p>
-                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil porro doloribus rem fugit distinctio et! Laudantium animi, possimus rem nam sint accusantium enim in nemo saepe, eaque soluta suscipit, provident culpa! Recusandae aliquid pariatur quisquam.</p>
-                                    <p className='font-bold text-xl mt-3'>Built with</p>
-                                    <div className="grid grid-cols-5 p-4">
-                                        <img className='h-14 w-auto' src={BootstrapIcon} alt="bootstrap icon" />
-                                        <img className='h-14 w-auto' src={ReactIcon} alt="" />
-                                        <img className='h-14 w-auto' src={CSharp} alt="c-sharp icon" />
-                                        <img className='h-14 w-auto' src={AzureIcon} alt="azure icon" />
-                                        <img className='h-14 w-auto' src={TypeScriptIcon} alt="typescript icon" />
+                                    <p className='font-bold text-xl smokeGreyText'>Description</p>
+                                    <p className='smokeyGrayText'>Pokedex uses the Pokémon API to display information about Pokémon. Users can search for Pokémon by name or ID and view details such as name, ID, image, type, location, evolutions, moves, abilities, and fun factoids. The app shows a random Pokémon when loaded and updates the display based on user searches. It includes features like adding/removing Pokémon from favorites and checking saved favorites. The app uses helper functions for data formatting, determining font color, retrieving flavor text, evolution sprites, moves, abilities, and location. Overall, it provides an intuitive interface for exploring and learning about Pokémon using the API.</p>
+                                    <p className='font-bold text-xl mt-3 smokeGreyText'>Built with</p>
+                                    <div className="grid grid-cols-5 p-4 mt-2">
+                                        <div className="flex flex-col justify-center">
+                                            <img className='h-14 w-auto' src={ReactIcon} alt="react icon" />
+                                            <p className='text-center'>react.js</p>
+                                        </div>
+                                        <div className="flex flex-col justify-center">
+                                            <img className='h-14 w-auto' src={TypeScriptIcon} alt="typescript icon" />
+                                            <p className='text-center'>typescript</p>
+                                        </div>
+                                        <div className="flex flex-col justify-center">
+                                            <img className='h-14 w-auto' src={TailwindIcon} alt="bootstrap icon" />
+                                            <p className='text-center'>tailwind</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className='grid grid-cols-3 gap-10 mt-5'>
+                            {/* <div className='grid grid-cols-4 gap-10 mt-5'>
                                 <div className='col-span-2'>
-                                    <img src={FleetFinderLoginImg} alt="image of Fleet Finder login page" />
+                                    <img src={PokeballHomepage} alt="image of Fleet Finder login page" />
                                 </div>
-                                <div className='row-span-2'>
-                                    <img src={FleetFinderCode} alt="image of Dispatch Dashboard from Fleet Finder app" />
+                                <div className='col-span-2'>
+                                    <img src={PokeballFavorites} alt="image of Dispatch Dashboard from Fleet Finder app" />
                                 </div>
-                                <div className='col-span-2 overflow-y-scroll h-72'>
-                                    <img className='w-full h-auto' src={DispatchDashboardImg} alt="image of code from Fleet Finder" />
-                                </div>
-                            </div>
-                            <hr className=' border-teal-300 rounded-3xl my-24' />
-                            <div className="grid grid-cols-2">
-
-                            </div>
+                            </div> */}
+                            <hr className=' border-teal-300 rounded-3xl my-8' />
                         </div>
                     </div>
                 </div>
--- src/components/NavbarFolder/NavbarComponent.tsx
+++ src/components/NavbarFolder/NavbarComponent.tsx
@@ -7,7 +7,7 @@ interface NavProps {
 const Navbar = () => {
     return(
         <>
-        <div id='nav' className="smokeyGray flex space-x-20 justify-between px-28 pt-14 pb-8 darkGreenBg h-20 fixed w-screen z-10">
+        <div id='nav' className="smokeyGrayText flex space-x-20 justify-between px-28 pt-14 pb-8 darkGreenBg h-20 fixed w-screen z-10">
             <a href="">ABOUT</a><a href="">PROJECTS</a><a href="">FLOW</a><a href="">CONTACT</a><a href="">RESUME</a><a href="">SOCIAL</a>
         </div>
         </>
